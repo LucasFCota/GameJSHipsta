4d312aae67388773fe11667ce949178b9b52cf0d
Lucas Flávio Cota <58710336+LucasFCota@users.noreply.github.com>
2020-07-14 15:44:11 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,9 @@ function setup() {
   let cnv = createCanvas(800, 600);
   cnv.position((windowWidth/2)-400, (windowHeight/2)-300);
   
-  frameRate(30);//30z
+  frameRate(30);//30Hz
+  somDoJogo.loop();
+  somDoJogo.setVolume(0.3);
   jogo = new Jogo();
   telaInicial = new TelaInicial();
   botaoGerenciador = new BotaoGerenciador('Iniciar', (windowWidth/2)-50, windowHeight*0.7);
@@ -20,9 +22,4 @@ function keyPressed(){
 
 function draw() {
   cenas[cenaAtual].draw();
-  let aux = 1;
-  if(aux){
-    somDoJogo.loop();
-    aux = 0;
-  }
 }
